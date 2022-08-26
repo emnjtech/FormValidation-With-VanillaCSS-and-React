cf6952747c381b599531df14c268ec1e0f579907
macbook pro i7 <macbookproi7@macbooks-MacBook-Pro.local>
2022-08-26 03:02:09 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import UserRegister from './UserRegister'
+
+function App() {
+
+ 
+
+  return (
+    <div>
+      <UserRegister />
+
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/dummyText.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function DummyText() {
+  return (
+    <div>
+    <div className='dummyContent'>
+    <h1>You couldn't have picked a better time to start.</h1>
+    <p>Nulla facilisi. Proin lacus est, gravida sed congue sed, 
+       vel pharetra massa elit nec lacus. Pellentesque 
+       habitant morbi tristique senectus et netus et malesuada fames ac 
+       Pellentesque laoreet vulputate laoreet.</p>
+    </div>
+    
+    </div>
+  )
+}
